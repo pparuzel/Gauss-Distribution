b7a1ac7021d6bb36880f9c44f72547bff37dcf71
Pawel Paruzel <pawelparuzel95@gmail.com>
2017-12-02 15:49:03 +0100

    Initial commit

---
+++ GaussDistrib/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GaussDistrib
+//
+//  Created by Pawel Paruzel on 02/12/2017.
+//  Copyright © 2017 Pawel Paruzel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
