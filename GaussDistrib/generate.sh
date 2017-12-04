e0a433d39e8c1ffc9b0bc2f5ea1a3066ca4734e1
Pawel Paruzel <pawelparuzel95@gmail.com>
2017-12-04 22:43:19 +0100

    Add scripts for plotting and launcher

--- GaussDistrib/generate.sh
+++ GaussDistrib/generate.sh
@@ -5,3 +5,10 @@
 #
 #  Created by Pawel Paruzel on 04/12/2017.
 #  Copyright © 2017 Pawel Paruzel. All rights reserved.
+
+mkdir -p build
+rm -f "Resources/graph.png"
+g++ -std=c++14 main.cpp -o "build/main.x"
+gnuplot plothist.gnu
+rm -Rf build
+open "Resources/graph.png"
